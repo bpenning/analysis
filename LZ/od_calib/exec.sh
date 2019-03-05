913d4a318d64f8957950acc493569a5ae36a0b5f
bpenning <penning@brandeis.edu>
2019-03-05 14:15:58 -0800

    upading script

--- LZ/od_calib/exec.sh
+++ LZ/od_calib/exec.sh
@@ -1,4 +1,3 @@
-#./configure.py --force /projecta/projectdirs/lz/data/MDC2/background/LZAP-3.10.0-PHYSICS-3.10.0/20170507/lz_2017050723_lzap.root
-./macro.py -n 100 --useProof  MyProcessor.C  RQfile_BG_3.10.0_11d.list
-
+#./configure.py --force /projecta/projectdirs/lz/data/MDC2/background/LZAP-3.12.0-PHYSICS-3.12.1/20170816/lz_2017081610_lzap.root
+./macro.py -n 100 od_calib.C  filelists/background_LZAP-3.12.0-PHYSICS-3.12.1.list
 nohup ./macro.py -n 1000 -m 10 --useProof   MyProcessor.C  RQfile_BG_3.10.0_11d.list  >output.dat &

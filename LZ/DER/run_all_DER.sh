84b91e3814a162efedaff0a8418ab43b4895079f
Bjoern Penning <penning@brandeis.edu>
2019-03-05 10:00:48 -0500

    updating DER run script

--- LZ/DER/run_all_DER.sh
+++ LZ/DER/run_all_DER.sh
@@ -1,3 +1,3 @@
 mkdir $1/DER
-for f in $/raw/job_*/*mctruth.root; do echo ./submit_DER.py -o $1/DER/ -f $f; done
+for f in $1/raw/job_*/*mctruth.root; do echo ./submit_DER.py -o $1/DER/ -f $f; done
 
